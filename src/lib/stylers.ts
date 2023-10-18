fd6b727665494595a504b37fd2804222855b046c
Woed E. Naz <leminnes@gmail.com>
2023-10-18 09:55:28 -0400

    Fixing Clearance styling

--- src/lib/stylers.ts
+++ src/lib/stylers.ts
@@ -1,36 +1,37 @@
 export interface StyleObject {
-  class: string;
+	class: string;
 }
 
 function toCssClassName(str: string): string {
-  let cssClass = str.replace(/[^a-z0-9]/gi, '-').toLowerCase();
-  if (/^\d/.test(cssClass)) cssClass = `n-${cssClass}`;
-  return cssClass;
+	let cssClass = str.replace(/[^a-z0-9]/gi, '-').toLowerCase();
+	if (/^\d/.test(cssClass)) cssClass = `n-${cssClass}`;
+	return cssClass;
 }
 
 export function cellStyle(value: string | boolean | null | undefined): string {
-  if (!value) return 'none';
-  if ( typeof value !== 'string' ) {
-    value = value.toString()
-  }
-  return value.match(/^\W/) ? `irregular-${toCssClassName(value)}` : toCssClassName(value);
+	if (!value) return 'none';
+	if ( typeof value !== 'string' ) {
+		value = value.toString()
+	}
+	return value.match(/^\W/) ? `irregular-${toCssClassName(value)}` : toCssClassName(value);
 }
 
-export function clearStyle(value: string | null | undefined): string {
-  const clearanceArray: string[] = ['unrestricted', 'restricted', 'confidential', 'secret', 'top-secret', 'cosmic-top-secret']
+export function clearStyle(
+	clearance: string | null | undefined
+): string {
+	const clear_class = (() => {
+		if (!clearance) return 'Unknown';
 
-  if (!value) return 'none';
-  if (value.match(/^[#0-9]\//)) {
-    value = value.split('/')[0];
-  }
-  if (value.match(/ [#0-9]$/)) {
-    value = value.split(' ')[1];
-  }
+		const clearanceLower = clearance.toLowerCase();
+		if (clearanceLower.includes('level 1')) return 'unrestricted';
+		if (clearanceLower.includes('level 2')) return 'restricted';
+		if (clearanceLower.includes('level 3')) return 'confidential';
+		if (clearanceLower.includes('level 4')) return 'secret';
+		if (clearanceLower.includes('level 5')) return 'top-secret';
+		if (clearanceLower.includes('level 6')) return 'cosmic-top-secret';
+		
+		return 'Unknown';
+	})();
 
-  const clearanceIndex: number = parseInt(value) - 1;
-  if (clearanceIndex >= 0 && clearanceIndex < clearanceArray.length) {
-    return clearanceArray[clearanceIndex];
-  }
-
-  return `irregular-clear-${value}`;
+	return clear_class;
 }
